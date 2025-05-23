2c91b3090d18260aa7236461e3e4c36749074df7
Silviu Ojog <sil.ojog@gmail.com>
2025-05-23 11:49:15 +0300

    Ziua 4

---
+++ Ziua4/retry.py
@@ -0,0 +1,9 @@
+
+is_success = False
+while not is_success:
+    x  = input("Introdu un numar")
+    if x == "2":
+        print("Mergem mai departe")
+        is_success = True
+    else:
+        print("Mai incearca")
